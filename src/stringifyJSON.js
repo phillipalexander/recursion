86067cad96c1f686dfed1830d8cbeb6beb874c3d
phillipalexander <git@phillipalexander.io>
2013-06-03 15:08:22 -0700

    stringifyJSON() - add case statements for incoming object types

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,49 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to have to write it from scratch:
-var stringifyJSON = function (obj) {
-  // your code goes here
+var stringifyJSON = function (object) {
+	var i, result, key, jsonKey, jsonValue;
+
+	switch (typeof object) {
+
+	case 'string':
+	case 'number':
+	case 'boolean':
+		return String(string);
+
+	case 'undefined':
+		return undefined;
+
+	case 'function':
+		// return null
+
+	case 'object':
+		//Null
+		if (!object) {
+			return 'null';
+		}
+
+		result = [];
+
+		//It's a girl/boy (array)!
+		if (Object.prototype.toString.apply(object) === '[object Array]') {
+			for (i = 0; i < object.length; i += 1) {
+				// result[i] = stringifyJSON(object[i]) || 'null';
+				result.push(stringifyJSON(object[i]) || 'null');
+			}
+			return "[" + result + "]";
+		} else {
+			//It's a boy/girl (object)!
+			for (key in object) {
+				if (Object.prototype.hasOwnProperty.call(object, key)) {
+					jsonKey = stringifyJSON(key);
+					jsonValue = stringifyJSON(object[key]);
+					result.push(jsonKey + ":" + jsonValue);
+				}
+			}
+			// return "{" + result.join() + "}";
+			return "{" + result + "}";
+		}
+
+	}
 };
