5da16084f9da1d72e9da313a2ad5c97fa7cf0462
phillipalexander <git@phillipalexander.io>
2013-06-09 11:58:37 -0700

    getElementsByClassName() - fix using Crockford's walkDOM() function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,50 @@
 // };
 
 // But in stead we're going to implement it from scratch:
+
+// var getElementsByClassName = function (className) {
+// };
+
+// But in stead we're going to implement it from scratch:
+// var getElementsByClassName = function (className) {
+// 	var result = [];
+// 	var focus = focus || document.body;
+// debugger;
+// 	if (focus.classList.contains(className)) {
+// 		result.push(focus);
+// 	}
+// 	if (focus.children.length) {
+// 		for (i = 0; i < focus.children.length; i++) {
+// 			getElementsByClassName(focus.children[i]);
+// 		}
+// 	}
+// 	return result;
+// };
+
+// // Crockford's Version (which uses firstChild() and nextSibling() methods :( )
+
 var getElementsByClassName = function (className) {
-  // your code here
+
+	var result = [];
+	// debugger;
+	var magic = function (node) {
+		if (node.classList) {
+			if (node.classList.contains(className)) {
+				result.push(node);
+			}
+		}
+	};
+	var walkDOM = function (node) {
+		magic(node);
+		node = node.firstChild;
+		while (node) {
+			walkDOM(node, magic);
+			node = node.nextSibling;
+		}
+
+	};
+
+	walkDOM(document.body);
+	return result;
+
 };
